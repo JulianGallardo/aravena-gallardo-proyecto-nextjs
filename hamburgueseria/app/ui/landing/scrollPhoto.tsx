b38ebf731acc2ac91f097d82e6e8a513c2b12902
Joaquin <joaco.aravena02@gmail.com>
2024-05-21 15:24:57 -0300

    scrolledPhoto avances

--- hamburgueseria/app/ui/landing/scrollPhoto.tsx
+++ hamburgueseria/app/ui/landing/scrollPhoto.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+'use client'
+import React, {useRef } from 'react';
 import Image from 'next/image';
 
 
 export default function ScrollPhoto() {
     const photoTitle = "Hamburguesas originales de Bahía Blanca";
     const photoSrc = "/landingPhoto.jpg";
+    const scrollDivRef = useRef<HTMLDivElement | null>(null);
 
+    const handleScrollDown = () => {
+            if (scrollDivRef.current) {
+                window.scrollTo({
+                    top: scrollDivRef.current.offsetHeight,
+                    behavior: 'smooth',
+                });
+            }
+        };
+      
     return (
-        <div className="hero min-h-screen">
-            <Image src={photoSrc} alt={photoTitle} fill={true}  objectFit='cover' objectPosition='' className="fill " />
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
+        <div className="h-screen w-screen overflow-hidden">
+            <Image src={`${photoSrc}`} alt="Background Image" layout="fill" objectFit="cover" style={{ display: 'none' }} />
+            <div
+                ref={scrollDivRef}
+                className="h-screen bg-cover bg-center cursor-pointer"
+                style={{ backgroundImage: `url(${photoSrc})` }}
+                onClick={handleScrollDown}
+            >
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
                     <h1 className="font-bold text-white sm:text-3xl md:text-5xl">{photoTitle}</h1>
                 </div>
             </div>
-        </div>
+            <div className="h-screen bg-gray-200">
+                <p className="text-center mt-40 z-50">Scroll down content...</p>
+            </div>
+        </div>  
     );
 }
