ed8509ae1b61e3901abea093d7e73142d23d0f3c
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-22 23:27:55 -0300

    implementado form de login

---
+++ hamburgueseria/app/(features)/login/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Header } from "@/app/ui";
+import { Footer } from "@/app/ui";
+import LoginForm from "@/app/ui/login/loginForm";
+
+const page: React.FC = () => {
+    return (
+        <div>
+            <Header isTransparent={true} />
+            <div className="flex flex-col items-center justify-center h-screen gap-5">
+                <LoginForm />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default page;
