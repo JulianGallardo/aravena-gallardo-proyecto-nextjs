a4727be5023817a818c6bdd01b11859c17d815dc
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-27 12:15:36 -0300

    correccion postcss

--- hamburgueseria/postcss.config.mjs
+++ hamburgueseria/postcss.config.mjs
@@ -1,9 +1,8 @@
-import { config } from 'process';
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: { config: '@/tailwind.config.ts' },
+    tailwindcss: {},
   },
 };
 
