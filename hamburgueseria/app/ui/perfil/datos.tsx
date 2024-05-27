dcf0f79cd95c33edb7169e229f98af2021bb4635
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-27 00:00:22 -0300

    cambios en btn perfil

--- hamburgueseria/app/ui/perfil/datos.tsx
+++ hamburgueseria/app/ui/perfil/datos.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
@@ -26,7 +26,7 @@ const Datos = () => {
         }
     }, [session]);
 
-    const handleChange = (e: { target: { id: any; value: any; }; }) => {
+    const handleChange = (e) => {
         const { id, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
@@ -34,7 +34,7 @@ const Datos = () => {
         }));
     };
 
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         update({
             ...session,
@@ -57,60 +57,62 @@ const Datos = () => {
                 editar ? (
                     <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
                         <h1 className="text-2xl font-bold text-lightgrey">Datos Personales</h1>
-                        <div className="flex flex-col text-sm text-lightgrey gap-2 md:grid grid-cols-2">
-                            <div>
-                                <label className="mb-3 mt-5 block text-xs font-medium">Nombre:</label>
-                                <input
-                                    id="fullName"
-                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
-                                    type="text"
-                                    value={formData.fullName}
-                                    onChange={handleChange}
-                                />
+                            <div className="flex flex-col text-sm text-lightgrey gap-2 md:grid grid-cols-2">
+                                <div>
+                                    <label className="mb-3 mt-5 block text-xs font-medium">Nombre:</label>
+                                    <input
+                                        id="fullName"
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
+                                        type="text"
+                                        value={formData.fullName}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div>
+                                    <label className="mb-3 mt-5 block text-xs font-medium">Email:</label>
+                                    <input
+                                        id="email"
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
+                                        type="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div>
+                                    <label className="mb-3 mt-5 block text-xs font-medium">Ciudad:</label>
+                                    <input
+                                        id="city"
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
+                                        type="text"
+                                        value={formData.city}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div>
+                                    <label className="mb-3 mt-5 block text-xs font-medium">Telefono:</label>
+                                    <input
+                                        id="cellphone"
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
+                                        type="text"
+                                        value={formData.cellphone}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div>
+                                    <label className="mb-3 mt-5 block text-xs font-medium">Direccion:</label>
+                                    <input
+                                        id="address"
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
+                                        type="text"
+                                        value={formData.address}
+                                        onChange={handleChange}
+                                    />
+                                </div>
                             </div>
-                            <div>
-                                <label className="mb-3 mt-5 block text-xs font-medium">Email:</label>
-                                <input
-                                    id="email"
-                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
-                                    type="email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div>
-                                <label className="mb-3 mt-5 block text-xs font-medium">Ciudad:</label>
-                                <input
-                                    id="city"
-                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
-                                    type="text"
-                                    value={formData.city}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div>
-                                <label className="mb-3 mt-5 block text-xs font-medium">Telefono:</label>
-                                <input
-                                    id="cellphone"
-                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
-                                    type="text"
-                                    value={formData.cellphone}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                            <div>
-                                <label className="mb-3 mt-5 block text-xs font-medium">Direccion:</label>
-                                <input
-                                    id="address"
-                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2 placeholder:text-gray-500"
-                                    type="text"
-                                    value={formData.address}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-                        <button type="submit" className="bg-dark text-white px-4 py-2 rounded-md hover:bg-darkblue">Guardar</button>
-                    </form>
+                            <button type="submit" className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Guardar</button>
+                        </form>
+                    </div>
+
                 ) : (
                     <div className="flex flex-col text-sm text-lightgrey gap-3">
                         <h1 className="text-2xl font-bold text-white">Datos Personales</h1>
@@ -121,7 +123,7 @@ const Datos = () => {
                             <p className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2">Telefono: {session.user?.cellphone}</p>
                             <p className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2">Direccion: {session.user?.address}</p>
                         </div>
-                        <button onClick={() => setEditar(true)} className="bg-dark text-white px-4 py-2 rounded-md hover:bg-darkblue">Editar</button>
+                        <button onClick={() => setEditar(true)} className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Editar</button>
                     </div>
                 )
             )}
--- hamburgueseria/app/ui/perfil/pedidosPaginacion.tsx
+++ hamburgueseria/app/ui/perfil/pedidosPaginacion.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import PedidosCard from "./pedidosCard";
-import { usePathname, useSearchParams } from "next/navigation";
+import PedidosCard from "@/app/ui/perfil/pedidosCard";
 
 const PedidosPaginacion = () => {
 
 
   return (
-    <div className="flex flex-col justify-center gap-5 md:grid grid-cols-2 ">
+    <div className="flex flex-col gap-5">
+      <h1 className="text-2xl font-bold text-lightgrey">Pedidos</h1>
+      <div className="flex flex-col justify-center gap-5 md:grid grid-cols-2 ">
 
         <PedidosCard />
         <PedidosCard />
         <PedidosCard />
         <PedidosCard />
-        <PedidosCard />
 
 
+      </div>
     </div>
   );
 };
