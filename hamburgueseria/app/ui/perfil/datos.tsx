79be3b2746b5b1c956bc501a81832cf3e9b9676e
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-27 00:03:54 -0300

    correccion error div

--- hamburgueseria/app/ui/perfil/datos.tsx
+++ hamburgueseria/app/ui/perfil/datos.tsx
@@ -55,8 +55,9 @@ const Datos = () => {
             {status === "loading" && <p>Cargando...</p>}
             {status === "authenticated" && (
                 editar ? (
-                    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
-                        <h1 className="text-2xl font-bold text-lightgrey">Datos Personales</h1>
+                    <div>
+                        <h1 className="text-2xl font-bold text-white">Datos Personales</h1>
+                        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-5">
                             <div className="flex flex-col text-sm text-lightgrey gap-2 md:grid grid-cols-2">
                                 <div>
                                     <label className="mb-3 mt-5 block text-xs font-medium">Nombre:</label>
@@ -109,7 +110,11 @@ const Datos = () => {
                                     />
                                 </div>
                             </div>
-                            <button type="submit" className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Guardar</button>
+                            <div className="items-center justify-center flex flex-row">
+
+                                <button type="submit" className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Guardar</button>
+
+                            </div>
                         </form>
                     </div>
 
@@ -123,7 +128,9 @@ const Datos = () => {
                             <p className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2">Telefono: {session.user?.cellphone}</p>
                             <p className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-2 pr-10 text-sm outline-2">Direccion: {session.user?.address}</p>
                         </div>
-                        <button onClick={() => setEditar(true)} className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Editar</button>
+                        <div className="items-center justify-center flex flex-row">
+                            <button onClick={() => setEditar(true)} className="bg-dark btn btn-circle text-white px-10 py-2 rounded-md w-fit hover:bg-darkblue">Editar</button>
+                        </div>
                     </div>
                 )
             )}
