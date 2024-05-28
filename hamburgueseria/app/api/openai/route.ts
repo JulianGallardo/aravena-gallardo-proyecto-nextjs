e68c6247bf860ef98ab5103c1b0541492221dbf2
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-28 14:14:03 -0300

    GEMINI RESPONDE

--- hamburgueseria/app/api/openai/route.ts
+++ hamburgueseria/app/api/openai/route.ts
@@ -1,35 +1,13 @@
-import OpenAi from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { streamText } from "ai";
+import {google} from '@ai-sdk/google'
 
-const openai = new OpenAi({
-    apiKey: process.env.OPENAI_API_KEY || '',
-});
-
-export const runtime = 'edge';
-
-export async function POST(
-    req: Request,
-    res: Response
-) {
+export async function POST(req: Request) {
     const { messages } = await req.json();
-    console.log("messages", messages);
-
-    const response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: [{
-            role: 'system',
-            content: 'Sos de ByteBurgers, una hamburgeseria que se especializa en hamburguesas de todo tipo, esta ubicada en Bahia Blanca, Buenos Aires'+
-                ', tu trabajo consiste en ayudar a las personas a elegir la hamburguesa que mas les guste, para eso tenes que hacerles preguntas sobre sus gustos y preferencias'+
-                ', y recomendarles la hamburguesa que mas se ajuste a sus gustos. Siempre respondes con respeto y amabilidad, y con tonada argentina, siempre tus respuestas son en menos de 500 caracteres.'
-
-        },
-
-        ],
-        stream: true,
-        temperature: 1,
+  
+    const result = await streamText({
+      model: google('models/gemini-pro'),
+      messages,
     });
-
-    const stream= OpenAIStream(response);
-
-    return new StreamingTextResponse(stream);
-}
+  
+    return result.toAIStreamResponse();
+  }
