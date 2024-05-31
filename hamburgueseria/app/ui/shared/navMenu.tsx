067853a00bdbcf441b9d57e2f0b8fbaf2c936ccb
Julian Gallardo <juliangallardo26603@gmail.com>
2024-05-31 00:41:58 -0300

    correccion estilos carrito

--- hamburgueseria/app/ui/shared/navMenu.tsx
+++ hamburgueseria/app/ui/shared/navMenu.tsx
@@ -179,16 +179,7 @@ const NavMenu: React.FC<NavMenuProps> = ({ isTransparent }) => {
                 </div>
                 <div className="navbar-end text-black">
                     <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost bg-lightgrey btn-circle" onClick={handleCartToggle}>
-                            <div className="indicator">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="black"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item"></span>
-                            </div>
-                        </div>
-                        {
-                            CartMenuShow &&
-                            <CartMenu handleCartClose={handleCartClose} />
-                        }
+                        <CartMenu handleCartClose={handleCartClose} handleCartToggle={handleCartToggle} CartMenuShow={CartMenuShow} />
                     </div>
                 </div>
             </div>
