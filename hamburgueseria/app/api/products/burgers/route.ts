4fee9329d6e2d4454148e3ac7dee7e36dc8ca6a7
Julian Gallardo <juliangallardo26603@gmail.com>
2024-06-02 00:05:41 -0300

    cambio en api para obtener de bdd

--- hamburgueseria/app/api/products/burgers/route.ts
+++ hamburgueseria/app/api/products/burgers/route.ts
@@ -3,55 +3,26 @@ import { Burger } from '@/prisma/generated/client';
 import { NextRequest, NextResponse } from "next/server";
 const productService = new ProductService();
 
-const burgersPlaceholder: Burger[] = [
-  {
-    burgerId: 1,
-    productId: 1,
-    name: "Classic Burger",
-    category: "SIMPLE",
-    description: "A delicious classic beef burger",
-    stock: 100,
-    price: 9.99
-  },
-  {
-    burgerId: 2,
-    productId: 2,
-    name: "Chicken Burger",
-    category: "SIMPLE",
-    description: "A tasty chicken burger",
-    stock: 100,
-    price: 8.99
-  },
-  {
-    burgerId: 3,
-    productId: 3,	
-    name: "Veggie Burger",
-    category: "SIMPLE",
-    description: "A flavorful vegetarian burger",
-    stock: 0,
-    price: 7.99
-  }
-];
+
 
 export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const productId = searchParams.get('productId');
     if (productId) {
         {
-            //const burger = await productService.getBurgerById(Number(productId));
+            const burger = await productService.getBurgerById(Number(productId));
+            return NextResponse.json({
+                status: 200,
+                body: burger
+            });
             //cambiar cuando usemos bdd
         }
-        const burger = burgersPlaceholder.find(burger => burger.productId === Number(productId));
-        console.log(burger);
-        return NextResponse.json({
-            status: 200,
-            body: burger
-        });
+        
     } else {
-        //const burgers = await productService.getAllBurgers();
+        const burgers = await productService.getAllBurgers();
         return NextResponse.json({
             status: 200,
-            body: burgersPlaceholder
+            body: burgers
         });
     }
 }
