6ccda1d37ea312a1ca85c0fed22ba715fc7eb34d
Julian Gallardo <juliangallardo26603@gmail.com>
2024-06-10 22:00:32 -0300

    agregado endpoint promos

--- hamburgueseria/app/ui/burgers/burgersList.tsx
+++ hamburgueseria/app/ui/burgers/burgersList.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 import { Card } from '@/app/ui';
@@ -15,7 +15,7 @@ const BurgerList: React.FC = () => {
     const fetchBurgers = async () => {
       try {
         const res = await fetch('/api/products/burgers', {
-          cache: 'reload', 
+          cache: 'reload',
         });
         if (!res.ok) {
           throw new Error('Network response was not ok');
@@ -24,47 +24,56 @@ const BurgerList: React.FC = () => {
         setBurgers(data.body);
 
         // Obtener las URL de las imágenes para cada burger
-        const urls: string[] = await Promise.all(data.body.map((burger: Burger) => getImageUrl(`${burger.burgerId}-${burger.name.replace(/\s/g, '')}`)));
+        const urls: string[] = await Promise.all(
+          data.body.map((burger: Burger) =>
+            getImageUrl(`${burger.burgerId}-${burger.name.replace(/\s/g, '')}`)
+          )
+        );
         setImageUrls(urls);
       } catch (error) {
         console.error('Error fetching burgers:', error);
       }
     };
+    
 
     fetchBurgers();
   }, []);
 
   return (
-    <div className='pt-16'>
-      {burgers.length !==0 && categories.map((category) => (
-        <ul id={category} key={category}>
-          <h2 className="text-2xl font-bold text-darkblue dark:text-white m-4 text-center">{category}</h2>
-          <div className="flex flex-wrap justify-center items-center">
-            {burgers.map((burger, index) => (
-              burger.category === category &&
-              <li key={burger.burgerId} className="w-64 m-4">
-                <Card
-                  title={burger.name}
-                  description={burger.description}
-                  photoSrc={
-                    //imageUrls[index]
-                    burger.imageUrl
-                  } // Obtener la URL de la imagen correspondiente
-                  price={burger.price}
-                  burgerId={burger.burgerId}
-                />
-              </li>
-            ))}
+    <div className="flex flex-col pt-16 px-4 items-center md:px-36 md:items-stretch  ">
+      {burgers.length !== 0 &&
+        categories.map((category) => (
+          <div key={category} className="mb-8">
+            <h2 className="text-2xl font-bold text-darkblue dark:text-white mb-4">
+              {category}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              {burgers.map(
+                (burger, index) =>
+                  burger.category === category && (
+                    <Card
+                      key={burger.burgerId}
+                      title={burger.name}
+                      description={burger.description}
+                      photoSrc={
+                        // imageUrls[index]
+                        burger.imageUrl
+                      } // Obtener la URL de la imagen correspondiente
+                      price={burger.price}
+                      burgerId={burger.burgerId}
+                    />
+                  )
+              )}
+            </div>
           </div>
-        </ul>
-      ))}
-      {
-        burgers.length === 0 && (
-          <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold text-darkblue dark:text-white">Cargando menu...</h1>
-          </div>
-        )
-      }
+        ))}
+      {burgers.length === 0 && (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-2xl font-bold text-darkblue dark:text-white">
+            Cargando menú...
+          </h1>
+        </div>
+      )}
     </div>
   );
 };
--- hamburgueseria/app/ui/shared/burgerInfoCard.tsx
+++ hamburgueseria/app/ui/shared/burgerInfoCard.tsx
@@ -13,14 +13,14 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, description, photoSrc, price, burgerId }) => {
     return (
         <Link href={`/burgers/${burgerId}`}>
-            <div className="transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl flex flex-col w-64 h-80 bg-white dark:text-darkblue shadow-lg rounded-lg overflow-hidden">
+            <div className="transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:shadow-black dark:hover:shadow-white flex flex-col w-64 h-80 bg-white dark:bg-gray-800 dark:text-white shadow-lg rounded-lg overflow-hidden">
                 <Image className="w-full h-32 object-cover object-center" src={photoSrc} alt="Card" width={500} height={200} />
                 <div className="px-6 py-4 flex-grow">
                     <h2 className="text-xl font-bold mb-2">{title}</h2>
-                    <p className="text-gray-500 text-base line-clamp-3">{description}</p>
+                    <p className="text-gray-500 dark:text-gray-300 text-base line-clamp-3">{description}</p>
                 </div>
                 <div className="px-6 py-4">
-                    <p className="text-green-500 text-xl font-semibold">${price}</p>
+                    <p className="text-green-500 dark:text-green-400 text-xl font-semibold">${price}</p>
                 </div>
             </div>
         </Link>
