b7df92dbe6d92dc88334557204a1c97d8429dc7b
Joaquin <joaco.aravena02@gmail.com>
2024-06-23 18:29:45 -0300

    admin puede subir fotos

--- hamburgueseria/app/ui/photos/uploadPhotos.tsx
+++ hamburgueseria/app/ui/photos/uploadPhotos.tsx
@@ -5,8 +5,6 @@ const UploadPhotos: React.FC = () => {
   const [image, setImage] = useState<string | ArrayBuffer | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [imageUrl, setImageUrl] = useState<string>("");
-  const [productId, setProductId] = useState<string>("");
-  const [name, setName] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -16,6 +14,7 @@ const UploadPhotos: React.FC = () => {
         setImage(reader.result);
       };
       reader.readAsDataURL(file);
+      handleUpload();
     }
   };
 
@@ -24,7 +23,7 @@ const UploadPhotos: React.FC = () => {
       setLoading(true);
       const res = await fetch("/api/images", {
         method: "POST",
-        body: JSON.stringify({ data: image, publicId: `${productId}-${name}` }),
+        body: JSON.stringify({ data: image, publicId: `${name}` }),
         headers: { "Content-Type": "application/json" },
       });
       const data = await res.json();
@@ -34,23 +33,9 @@ const UploadPhotos: React.FC = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6  rounded-lg shadow-md border ">
-      <h2 className="text-2xl font-bold mb-6 text-center">Upload Image</h2>
+    <div>
+      <label htmlFor="name">Foto</label>
       <div className="space-y-4">
-        <input
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          type="text"
-          value={productId}
-          onChange={(e) => setProductId(e.target.value)}
-          placeholder="Product ID"
-        />
-        <input
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-        />
         <label className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer flex items-center justify-center bg-gray-100 hover:bg-gray-200">
           <span className="text-gray-600">{image ? "File Selected" : "Select a file"}</span>
           <input
@@ -60,13 +45,6 @@ const UploadPhotos: React.FC = () => {
             onChange={handleChange}
           />
         </label>
-        <button
-          className={`w-full p-2 rounded ${loading ? "bg-gray-400" : "bg-blue-500 hover:bg-blue-600"} text-white font-bold`}
-          onClick={handleUpload}
-          disabled={loading}
-        >
-          {loading ? "Uploading..." : "Upload"}
-        </button>
       </div>
       {imageUrl && (
         <div className="mt-6 text-center">
