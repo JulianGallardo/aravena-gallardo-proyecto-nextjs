21fc7a81f6943ece384c987b09e028406ebc4355
Julian Gallardo <juliangallardo26603@gmail.com>
2024-06-23 21:54:21 -0300

    entrega

--- hamburgueseria/app/ui/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './shared/header';
-export { default as Footer } from './shared/footer';
-export { default as Card } from './shared/burgerInfoCard';
-export { default as Button } from './shared/button';
-export { default as Categorias } from './pedido/categorias';
-export { default as BurgerList } from './burgers/burgersList';
--- hamburgueseria/prisma/migrations/20240613223218_merge/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Burger" ADD COLUMN     "imageUrl" TEXT NOT NULL DEFAULT 'https://cache-mcd-ecommerce.appmcdonalds.com/images/AR/DLV_48040_v9.png';
